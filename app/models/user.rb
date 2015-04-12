77bc0a172e3f270e49b8e9d303d17e3642ae16b1
Sean Hunter <jamesb2147@gmail.com>
2015-04-12 13:19:59 -0400

    Fixed user creation and hid password digest when displaying user models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-has_many :trips
 has_secure_password
+has_many :trips
 #attr_accessor :password, :password_confirmation are created with has_secure_password
 
 validates :email, presence: true, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
@@ -8,15 +8,14 @@ validates :given_name, presence: true
 validates :family_name, presence: true
 validates :country, presence: true, format: { with: /\A[A-Za-z][A-Za-z]\z/ }
 
-def generate_password_reset_token
-	update_attribute(password_reset_token, SecureRandom.urlsafe_base64(48))
-end
+before_save { downcase_email }
 
-#Have to do a case-insensitive email check, so easier to just downcase everything. Sorry, users.
 def downcase_email
 	self.email = email.downcase
 end
 
-before_save downcase_email
+def generate_password_reset_token!
+	update_attribute(password_reset_token, SecureRandom.urlsafe_base64(48))
+end
 
 end
